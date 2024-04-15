a6214e368e0cc5f732ad3d259846a0766b6df339
miranda404 <menezestrove@gmail.com>
2024-04-15 08:58:18 -0300

    Adicionadas as chamadas das historias,comentarios e descricao

--- main.cpp
+++ main.cpp
@@ -1,16 +1,58 @@
 #include <iostream>
 #include <string>
-
+#include "produtos.hpp"
 
 
 
 int main(){
+    
+    //Executavel
+    
+    //DESCONTO
+    std::cout<<"Bem vindo(a) a loja virtual!"<<std::endl;
+    std::cout<<"Voce deseja ver os produtos com desconto? [1]Sim/[2]Nao"<<std::endl;
+    
+    int desconto_desejado=0;
+    std::cin>>desconto_desejado;
 
+    if(desconto_desejado!=1||desconto_desejado!=0)
+    {
+        std::cout<<"ERRO! OPCAO INVALIDA"<<std::endl;
+    }
+    else if (desconto_desejado==1)
+    {
+        //CHAMADA DA FUNÇÃO FILTRAGEM POR PREÇO
+    }
+    else{
+        
+        //TIPO DO PRODUTO
+        std::cout<<"Qual o tipo de produto que voce busca?"<<std::endl;
+        std::cout<<"Eletrodomestico,Decoracao ou Movel"<<std::endl;
+    
+        std::string tipo_desejado;
+        std::cin>>tipo_desejado;
 
+        if(tipo_desejado!="Eletrodomestico"||tipo_desejado!="Decoracao"||tipo_desejado!="Movel"){
+        std::cout<<"ERRO! OPCAO INVALIDA"<<std::endl;
+        }
+        else{
+            //CHAMADA DA FUNÇÃO DE FILTRAGEM POR NOME
+        }
+    
+        //MARGEM DE PREÇO
+        std::cout<<"Qual a margem de preco do produto que voce busca?"<<std::endl;
+        std::cout<<"[1](0-50),[2](51-100),[3](101-150),[4](200+)"<<std::endl;
 
+        int valor_desejado=0;
+        std::cin>>valor_desejado;
 
-
-
+        if(valor_desejado<0||valor_desejado>4){
+        std::cout<<"ERRO! VALOR INVALIDO"<<std::endl;
+        }
+        else{
+            //CHAMADA DA FUNÇÃO DE FILTRAGEM POR PRECO
+        }
+    }
 
 
 
--- produtos.hpp
+++ produtos.hpp
@@ -7,9 +7,10 @@ class PRODUTO{
     
     //IMPLEMENTAÇÃO
     private:
-        std::string nome;
-        int faixa_de_preco=0;
-        int quantidade=0;
+        std::string produto_nome;
+        std::string produto_tipo;
+        int produto_preco=0;
+        int produto_estoque=0;
 
     public:
     //INTERFACE
@@ -22,9 +23,23 @@ class PRODUTO{
             if (!arquivo.is_open()){
                 std::cout<<"Erro ao abrir o arquivo"<<std::endl;
             }
-
-
         }
+            
+            
+        //leitura do arquivo
+
+        //cin>>produto_nome
+        //cin>>produto_tipo      todas essas se jutnarão em registrar()
+        //cin>>produto_preco
+        //cin>>produto_estoque
+
+        //fechar arquivo
+
+        /*Serão necessários métodos para as atribuições, para display dos itens da implementação,
+        por exemplo um método void atribuir_estoque() que manipulará o atributo produto_estoque
+        e outra como void imprimir_estoque(), estes métodos serão agrupados em métodos que atribuirão/
+        imprimirão todas as informações de uma vez*/
+        
 
 
 };
