199045afee4e994c5d4211ac00ad7d5a6b007667
miranda404 <menezestrove@gmail.com>
2024-04-16 21:34:19 -0300

    Adicionada as funções de leitura do arquivo e registro em um vetor, agora resta criar um método para dispor os objetos em uma matriz tridmensional ou diferentes vetores

--- README.txt
+++ README.txt
@@ -3,3 +3,14 @@ o catálogo dos itens será feito por meio da leitura de um arquivo txt, nele co
 produto, a faixa de preço, o tipo de produto, a quantidade de produtos disponível no estoque 
 e se eles estão ou não com desconto. O usuário poderá escolher a faixa de preço dos produtos 
 e filtra-los por tipo (eletrodoméstico, moveis, decoração, etc...).
+
+
+
+
+As categorias são: eletrodoméstico[1],moveis[2],decoração[3].
+As faixas de preço são: [1-50],[51-100],[101-150],[200+].
+
+os objetos devem ser listados como:
+nome categoria[1-3] faixa de preço[1-4] quantidade no estoque
+
+para as estruturas de iteração funcionarem devem ser espaçados por um " " apenas e uma quebra de linha por item
--- produtos.hpp
+++ produtos.hpp
@@ -1,31 +1,81 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 
 class PRODUTO{
     
     //IMPLEMENTAÇÃO
     private:
-        std::string produto_nome;
-        std::string produto_tipo;
-        int produto_preco=0;
-        int produto_estoque=0;
+        std::string nome;
+        int tipo=0;                  //1=eletrodomesticos,2=movel,3=decoracao
+        int faixa_preco=0;
+        int estoque=0;
 
     public:
     //INTERFACE
 
         //ABRIR ARQUIVO PARA LEITURA
-        void abrir_arquivo(){
-            std::string nome_arquivo = "catalogo.txt";
-            std::ifstream arquivo(nome_arquivo);
-            //verificar abertura
-            if (!arquivo.is_open()){
-                std::cout<<"Erro ao abrir o arquivo"<<std::endl;
+        void ler_arquivo(){
+            std::ifstream arq("C:\\Users\\lucas\\Desktop\\TRABALHO\\catalogo.txt");      //CRIADA DE UM OBJETO DA CLASSE IFSTREAM
+            if(!arq){
+                std::cout<<"ERRO AO ABRIR O ARQUIVO!"<<std::endl;       
             }
+            
+            else{   //LEITURA
+                std::vector<PRODUTO> produtos;      
+                PRODUTO temp;
+                while(arq>>temp.nome>>temp.tipo>>temp.faixa_preco>>temp.estoque){  //Enquanto ler, atribuir a temp
+                    produtos.push_back(temp);       //Aqui, ele coloca cada objeto dentro de um vetor <produtos>
+                }
+
+                arq.close();           //FECHA O ARQUIVO
+
+                /*for(int i=0;i<produtos.size();i++){     //PARA TESTES
+                    std::cout<<produtos[i].nome<<" "<<produtos[i].tipo<<std::endl;
+                }*/
+
+                    //ORGANIZAR ARQUIVO
+                std::vector<std::vector<std::vector<PRODUTO>>> matriz_de_produtos;      //Ela terá que ser tridmensional já que existem objetos c mesmo tipo/preco
+                    for(int i=0;i<)
+
+
+
+            }
+
+
+            
         }
+        
+        
+        
+
+
+
+
+        };
             
-            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         //leitura do arquivo
 
         //cin>>produto_nome
@@ -42,4 +92,3 @@ class PRODUTO{
         
 
 
-};
